8ce75850120f6d8ad836b4dff3ade10d843a98ff
kouya17 <17383696+kouya17@users.noreply.github.com>
2019-03-10 20:31:38 +0900

    Fix: D-sub通信をI2Cに集約

--- slave_template.ino
+++ slave_template.ino
@@ -4,8 +4,6 @@
 #include "debug.h"
 #include "dsub_slave_communicator.hpp"
 
-#define PIN_GOAL            4   //ゴール判定用ピン
-#define PIN_HIT             5   //当たった判定用ピン
 #define PIN_GOAL_SENSOR     6   //通過/ゴールしたことを検知するセンサのピン
 #define PIN_HIT_SENSOR      7   //当たったことを検知するセンサのピン
 #define PIN_DIP_0           6   //DIPスイッチbit0
@@ -14,15 +12,12 @@
 #define PIN_DIP_3           9   //DIPスイッチbit3
 
 /* 変数宣言 */
-unsigned char active = 0; //0のときこのモジュール内にいない/1のときこのモジュール内にいる
-unsigned char slv_address; //スレーブアドレス(0はゴールモジュール固定、それ以外は1~)
+unsigned char slv_address;      //  スレーブアドレス(0はゴールモジュール固定、それ以外は1~)
 DsubSlaveCommunicator *dsubSlaveCommunicator = NULL;
 
 void setup(void) {
   /* ここから各スレーブ共通コード */
-  Serial.begin(57600);          //デバッグ用
-  pinMode(PIN_GOAL, INPUT); //通過判定用ピンを入力として設定
-  pinMode(PIN_HIT, INPUT);     //当たった判定用ピンを入力として設定
+  BeginDebugPrint();                //  デバッグ用
   /* ディップスイッチを入力として設定 */
   pinMode(PIN_DIP_0, INPUT);
   pinMode(PIN_DIP_1, INPUT);
@@ -31,8 +26,7 @@ void setup(void) {
   
   slv_address = ReadDipSwitch(); //SLVアドレスを設定
   /* D-sub通信管理用インスタンスの生成 */
-  dsubSlaveCommunicator = new DsubSlaveCommunicator(PIN_GOAL_SENSOR, PIN_HIT_SENSOR,
-                                                    PIN_GOAL, PIN_HIT, slv_address);
+  dsubSlaveCommunicator = new DsubSlaveCommunicator(PIN_GOAL_SENSOR, PIN_HIT_SENSOR, slv_address);
   //Wire.begin(slv_address);     //スレーブアドレスをslv_addressとしてI2C開始
   /* ここまで各スレーブ共通コード */
 
